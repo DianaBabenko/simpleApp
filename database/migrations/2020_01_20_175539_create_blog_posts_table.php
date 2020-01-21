59e14e5e4436037e0b3759cc101090e89b44832e
DianaBabenko <baben.k.o.diana2017@gmail.com>
2020-01-21 18:18:17 +0200

    init

---
+++ database/migrations/2020_01_20_175539_create_blog_posts_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBlogPostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blog_posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->integer('category_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+
+            $table->string('slug')->unique();
+            $table->string('title');
+
+            $table->text('excerpt')->nullable();
+
+            $table->text('content_raw');
+            $table->text('content_html');
+
+            $table->boolean('is_published')->default(false);
+            $table->timestamp('published_at')->nullable();
+
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('category_id')->references('id')->on('blog_categories');
+            $table->index('is_published');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blog_posts');
+    }
+}
---
+++ database/seeds/BlogCategoriesTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class BlogCategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $categories = [];
+
+        $cName = 'Без категории';
+        $categories[] = [
+            'title' => $cName,
+            'slug' => str_shuffle($cName),
+            'parent_id' => 0,
+        ];
+    }
+}
