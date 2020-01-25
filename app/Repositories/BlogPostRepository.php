d1ddff7547fbf11e60bd58e8e878b83cec02d735
DianaBabenko <baben.k.o.diana2017@gmail.com>
2020-01-25 15:29:30 +0200

    add repository for posts

---
+++ app/Repositories/BlogPostRepository.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\BlogPost as Model;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Pagination\LengthAwarePaginator;
+
+/**
+ * Class BlogPostRepository
+ *
+ * @package App\Repositories
+ */
+class BlogPostRepository extends CoreRepository
+{
+    /**
+     * @return string
+     */
+    protected function getModelClass()
+    {
+        return Model::class;
+    }
+
+    /**
+     * @return LengthAwarePaginator
+     */
+    public function getAllWithPaginate()
+    {
+        $columns = [
+            'id',
+            'title',
+            'slug',
+            'is_published',
+            'published_at',
+            'user_id',
+            'category_id'
+        ];
+
+        $result = $this->startConditions()
+            ->select($columns)
+            ->orderBy('id', 'DESC')
+            ->paginate(25);
+
+        return $result;
+    }
+}
