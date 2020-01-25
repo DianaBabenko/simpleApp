d8b7ebb550f8f1efbc0aa0ad5aebe33cd94f806c
DianaBabenko <baben.k.o.diana2017@gmail.com>
2020-01-25 15:27:41 +0200

    add routes for posts

--- routes/web.php
+++ routes/web.php
@@ -34,6 +34,11 @@ Route::group($groupData, function() {
     Route::resource('categories', 'CategoryController')
         ->only($methods)
         ->names('blog.admin.categories');
+
+    //BlogPost
+    Route::resource('posts', 'PostController')
+        ->except(['show'])
+        ->names('blog.admin.posts');
 });
 
 
